b40b1e9febc2177a1139884bb7a695778e004fd2
zbirenbaum <zacharyobirenbaum@gmail.com>
2023-07-15 22:30:41 -0700

    working completions handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use serde_json::Value;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
 mod reader;
 mod completions;
 mod auth;
@@ -5,10 +8,11 @@ use std::fs::File;
 use tower_lsp::jsonrpc::Result;
 use serde::{Deserialize, Serialize};
 use tower_lsp::lsp_types::*;
-use tower_lsp::{Client, LanguageServer, LspService, Server};
+use tower_lsp::lsp_types::notification::LogMessage;
 use futures_util::stream::StreamExt;
 use reqwest::RequestBuilder;
 use eventsource_stream::{Eventsource, EventStream};
+use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
 
 struct Copilot {
  client: Client,
@@ -44,15 +48,17 @@ struct Document {
 
 #[derive(Deserialize, Serialize, Debug,)]
 #[serde(rename_all = "camelCase")]
-struct CompletionParams {
+struct CopilotCompletionParams {
   doc: Document,
   position: Position,
   text_document: TextDocument,
 }
 
 impl Copilot {
-  async fn get_completions_cycling(&self, params: CompletionParams) {
+  async fn get_completions_cycling(&self, params: CopilotCompletionParams) {
+    self.client.log_message(MessageType::ERROR, "FUCKYES").await;
     let data = get_test_request();
+    self.client.log_message(MessageType::ERROR, "Here bby").await;
     let mut stream = self.fetcher.request(data)
       .send()
       .await.unwrap()
@@ -60,12 +66,11 @@ impl Copilot {
       .eventsource();
     while let Some(event) = stream.next().await {
       match event {
-        Ok(event) => println!(
-          "received event[type={}]: {}",
-          event.event,
-          event.data
-        ),
-        Err(e) => eprintln!("error occured: {}", e),
+        Ok(event) => {
+          println!( "received event[type={}]: {}", event.event, event.data);
+          self.client.log_message(MessageType::ERROR, "FUCKYES").await;
+        },
+        Err(e) => println!("error occured: {}", e),
       }
     }
   }
@@ -75,21 +80,37 @@ impl Copilot {
 #[tower_lsp::async_trait]
 impl LanguageServer for Copilot {
   async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-    println!("initialize");
     Ok(InitializeResult {
       server_info: None,
       capabilities: ServerCapabilities {
-        text_document_sync: None,
-        completion_provider: None,
-        execute_command_provider: None,
-        workspace: None,
+        text_document_sync: Some(TextDocumentSyncCapability::Kind(
+          TextDocumentSyncKind::INCREMENTAL,
+        )),
+        completion_provider: Some(CompletionOptions {
+          resolve_provider: Some(false),
+          trigger_characters: Some(vec![".".to_string()]),
+          work_done_progress_options: Default::default(),
+          all_commit_characters: None,
+          ..Default::default()
+        }),
+        execute_command_provider: Some(ExecuteCommandOptions {
+          commands: vec!["dummy.do_something".to_string()],
+          work_done_progress_options: Default::default(),
+        }),
+        workspace: Some(WorkspaceServerCapabilities {
+          workspace_folders: Some(WorkspaceFoldersServerCapabilities {
+            supported: Some(true),
+            change_notifications: Some(OneOf::Left(true)),
+          }),
+          file_operations: None,
+        }),
         ..ServerCapabilities::default()
       },
+      ..Default::default()
     })
   }
 
   async fn initialized(&self, _: InitializedParams) {
-    println!("initialize");
     self.client
       .log_message(MessageType::INFO, "initialized!")
       .await;
@@ -97,6 +118,69 @@ impl LanguageServer for Copilot {
 
   async fn shutdown(&self) -> Result<()> {
     Ok(())
+  }
+
+  async fn did_change_workspace_folders(&self, _: DidChangeWorkspaceFoldersParams) {
+    self.client
+      .log_message(MessageType::INFO, "workspace folders changed!")
+      .await;
+  }
+
+  async fn did_change_configuration(&self, _: DidChangeConfigurationParams) {
+    self.client
+      .log_message(MessageType::INFO, "configuration changed!")
+      .await;
+  }
+
+  async fn did_change_watched_files(&self, _: DidChangeWatchedFilesParams) {
+    self.client
+      .log_message(MessageType::INFO, "watched files have changed!")
+      .await;
+  }
+
+  async fn execute_command(&self, _: ExecuteCommandParams) -> Result<Option<Value>> {
+    self.client
+      .log_message(MessageType::INFO, "command executed!")
+      .await;
+
+    match self.client.apply_edit(WorkspaceEdit::default()).await {
+      Ok(res) if res.applied => self.client.log_message(MessageType::INFO, "applied").await,
+      Ok(_) => self.client.log_message(MessageType::INFO, "rejected").await,
+      Err(err) => self.client.log_message(MessageType::ERROR, err).await,
+    }
+
+    Ok(None)
+  }
+
+  async fn did_open(&self, _: DidOpenTextDocumentParams) {
+    self.client
+      .log_message(MessageType::INFO, "file opened!")
+      .await;
+  }
+
+  async fn did_change(&self, _: DidChangeTextDocumentParams) {
+    self.client
+      .log_message(MessageType::INFO, "file changed!")
+      .await;
+  }
+
+  async fn did_save(&self, _: DidSaveTextDocumentParams) {
+    self.client
+      .log_message(MessageType::INFO, "file saved!")
+      .await;
+  }
+
+  async fn did_close(&self, _: DidCloseTextDocumentParams) {
+    self.client
+      .log_message(MessageType::INFO, "file closed!")
+      .await;
+  }
+
+  async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
+    Ok(Some(CompletionResponse::Array(vec![
+      CompletionItem::new_simple("Hello".to_string(), "Some detail".to_string()),
+      CompletionItem::new_simple("Bye".to_string(), "More detail".to_string()),
+    ])))
   }
 }
 
@@ -107,17 +191,16 @@ async fn main() {
   let builder = auth::get_request_builder(&copilot_token).unwrap();
   let fetcher = completions::CompletionFetcher::new(builder);
 
-  // #[cfg(feature = "runtime-agnostic")]
-  // use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
-
-  // tracing_subscriber::fmt().init();
-
+  tracing_subscriber::fmt().init();
   let (stdin, stdout) = (tokio::io::stdin(), tokio::io::stdout());
-  // #[cfg(feature = "runtime-agnostic")]
-  // let (stdin, stdout) = (stdin.compat(), stdout.compat_write());
+  #[cfg(feature = "runtime-agnostic")]
+  let (stdin, stdout) = (stdin.compat(), stdout.compat_write());
+
 
   let (service, socket) = LspService::build(|client| Copilot { client, fetcher })
     .custom_method("getCompletionsCycling", Copilot::get_completions_cycling)
+    .custom_method("getCompletions", Copilot::get_completions_cycling)
+    .custom_method("TextDocument/completions", Copilot::get_completions_cycling)
     .finish();
   println!("Listening on stdin/stdout");
   Server::new(stdin, stdout, socket).serve(service).await;
